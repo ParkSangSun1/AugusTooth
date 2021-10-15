741e59dffbe760e63f921a68ec0cefe9f88af918
ParkSangSun <appleasz1028102@gmail.com>
2021-10-15 09:53:54 +0900

    [UPDATE] Splash Fragment -> Activity

---
+++ presentation/src/main/java/com/kdn/presentation/view/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.kdn.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kdn.presentation.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
