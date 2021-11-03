7ee17d5ac9d26e9e0024474b35eabc4ec105d2c4
ParkSangSun <appleasz1028102@gmail.com>
2021-11-03 12:07:38 +0900

    [ADD] DataSource

---
+++ data/src/main/java/com/pss/data/repository/datasourcelmpl/KakaoAddressDataSourceImpl.kt
@@ -0,0 +1,25 @@
+package com.pss.data.repository.datasourcelmpl
+
+import com.pss.data.api.KakaoAddressApi
+import com.pss.data.model.kakao.response.SearchAddress
+import com.pss.data.repository.datasource.KakaoAddressDataSource
+import retrofit2.Response
+import javax.inject.Inject
+
+class KakaoAddressDataSourceImpl @Inject constructor(
+    private val searchAddressApi: KakaoAddressApi,
+) : KakaoAddressDataSource {
+    override suspend fun searchAddress(
+        Authorization: String,
+        analyze_type: String,
+        page: Int,
+        size: Int,
+        query: String
+    ): List<SearchAddress> = searchAddressApi.searchAddress(
+        Authorization = Authorization,
+        analyze_type = analyze_type,
+        page = page,
+        size = size,
+        query = query
+    )
+}
