3da8bf237e1e1d8a04d017e3d60219d6fc1b5095
ParkSangSun <appleasz1028102@gmail.com>
2021-11-03 12:07:18 +0900

    [ADD] KakaoApi

---
+++ data/src/main/java/com/pss/data/api/KakaoAddressApi.kt
@@ -0,0 +1,19 @@
+package com.pss.data.api
+
+import com.pss.data.model.kakao.response.SearchAddress
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface KakaoAddressApi {
+    @GET("/v2/local/search/address.json")
+    suspend fun searchAddress(
+        @Header("Authorization") Authorization: String,
+        @Query("analyze_type") analyze_type: String,
+        @Query("page") page: Int,
+        @Query("size") size: Int,
+        @Query("query") query: String
+
+    ) : List<SearchAddress>
+}
