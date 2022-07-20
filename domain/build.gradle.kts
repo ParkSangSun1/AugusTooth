e4fdce2e3099113a1e5b6f437582b9b12b21f26f
ParkSangSun <appleasz1028102@gmail.com>
2022-07-20 19:26:01 +0900

    [REFACTORING] Domain 레이어 불필요한 종속성 제거

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,44 +1,9 @@
 plugins {
-    id ("com.android.library")
-    id ("kotlin-android")
+    id ("org.jetbrains.kotlin.jvm")
     id ("kotlin-kapt")
-    id ("dagger.hilt.android.plugin")
-}
-
-android {
-    compileSdk = SdkVersions.compileSdk
-
-    defaultConfig {
-        minSdk = SdkVersions.minSdk
-        targetSdk = SdkVersions.targetSdk
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            isTestCoverageEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
-    implementation (KTX.CORE)
-    implementation (AndroidX.APP_COMPAT)
-    implementation (Google.MATERIAL)
-    androidTestImplementation (TestTool.EXT_JUNIT)
-    androidTestImplementation (TestTool.ANDROID_JUNIT)
-    androidTestImplementation (TestTool.ANDROID_ESPRESSO)
 
     // Retrofit
     implementation (Retrofit.RETROFIT)
@@ -50,8 +15,5 @@ dependencies {
     implementation (OkHttp.LOGGING_INTERCEPTOR)
 
     // dager hilt
-    implementation (DaggerHilt.DAGGER_HILT)
-    kapt (DaggerHilt.DAGGER_HILT_COMPILER)
-    implementation (DaggerHilt.DAGGER_HILT_VIEW_MODEL)
-    kapt (DaggerHilt.DAGGER_HILT_ANDROIDX_COMPILER)
+    implementation (DaggerHilt.DAGGER_HILT_JAVAX)
 }
