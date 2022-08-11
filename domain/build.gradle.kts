bca902f3932ead0e782535ab2b63490e8bd5b1fd
ParkSangSun <appleasz1028102@gmail.com>
2022-08-11 22:47:38 +0900

    [REFACTORING] Domain layer 사용하지 않는 라이브러리 제거

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -5,15 +5,6 @@ plugins {
 
 dependencies {
 
-    // Retrofit
-    implementation (Retrofit.RETROFIT)
-    implementation (Retrofit.CONVERTER_GSON)
-    implementation (Retrofit.CONVERTER_JAXB)
-
-    //okHttp
-    implementation (OkHttp.OKHTTP)
-    implementation (OkHttp.LOGGING_INTERCEPTOR)
-
     // dager hilt
     implementation (DaggerHilt.DAGGER_HILT_JAVAX)
 }
